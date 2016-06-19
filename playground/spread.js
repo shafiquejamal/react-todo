f9984bc59fb6669e70c82dfc188a74904db0e49c
Shafique Jamal <shafique.jamal@gmail.com>
2016-06-19 14:15:22 -0400

    Add playgroud for spread operator (does not work yet)

---
+++ playground/spread.js
@@ -0,0 +1,31 @@
+// // function add (a, b) {
+// //   return a + b;
+// // }
+// //
+// // console.log(add(3, 1));
+// //
+// // var toAdd = [9, 5];
+// // console.log(add(...toAdd));
+// //
+// // var groupA = ['Alice', 'Cory'];
+// // var groupB = ['Vikram'];
+// // var final = [...groupB, 3, ...groupA];
+// //
+// // console.log(final);
+//
+// var person = ['Alice', 25];
+// var personTwo = ['Jen', 29];
+//
+// function greet (name, age) {
+//   console.log('Hello ' + name + ', you are ' + age);
+// }
+//
+// greet(...person);
+// greet(...personTwo);
+//
+// var names = ['Mike', 'Ben'];
+// var final = ['Shafique', ...names];
+//
+// final.forEach ( (name) => {
+//   console.log('Hello ' + name);
+// } );
