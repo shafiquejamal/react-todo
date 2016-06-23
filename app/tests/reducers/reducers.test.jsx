1cdf4aa2b653e77461e2d452dda317d60da196d3
Shafique Jamal <shafique.jamal@gmail.com>
2016-06-23 16:31:34 -0400

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,6 @@
 var expect = require('expect');
 var df = require('deep-freeze-strict');
+var moment = require('moment');
 
 var reducers = require('reducers');
 
@@ -30,8 +31,65 @@ describe('Reducers', () => {
       var response2 = reducers.showCompletedReducer(df(true), df(action));
       expect(response2).toBe(false);
     });
-
-
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'walk the dog'
+      };
+      var response = reducers.todosReducer(df([]), df(action));
+
+      expect(response.length).toEqual(1);
+      expect(response[0].text).toEqual(action.text);
+    });
+
+    it('should toggle completed and set completedAt', () => {
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 2
+      }
+
+      var todos = [
+        {
+          id: 1,
+          text: 'one',
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }, {
+          id: 2,
+          text: 'two',
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }, {
+          id: 3,
+          text: 'three',
+          completed: true,
+          createdAt: moment().unix(),
+          completedAt: moment().unix()
+        }
+      ];
+
+      var action2 = {
+        type: 'TOGGLE_TODO',
+        id: 3
+      }
+
+      var response = reducers.todosReducer(df(todos), df(action));
+
+      expect(response[1].completed).toBe(true);
+      expect(response[1].completedAt).toExist();
+
+      var response2 = reducers.todosReducer(df(todos), df(action2));
+
+      expect(response2[2].completed).toBe(false);
+      expect(response2[2].completedAt).toNotExist();
+
+    });
+  });
+
 
 });
